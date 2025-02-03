4ac358dc6777781e7ab334691f1cba502793b493
Sandra-k-pradeep <93859982+Sandra-k-pradeep@users.noreply.github.com>
2025-02-03 11:04:58 +0530

    User authentication

--- web.php
+++ web.php
@@ -1,25 +1,41 @@
 <?php
-namespace App\Http\Controllers\ProductController;
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\task;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\HomeController;
 
+// Default welcome page
+Route::get('/', function () {
+    return view('welcome');
+});
 
+// Authentication Routes
+Auth::routes();
 
+// Home route after login
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
+// Login Routes
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Registration Routes
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
 
+// Password Reset Routes
+Route::get('/password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('/password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('/password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('/password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
 
+// Protect Dashboard (Only logged-in users can access)
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth')->name('dashboard');
 
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
-Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
